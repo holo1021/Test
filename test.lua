bdfe5eae6aaca81aca38bf435a8f63d008b534b0
holo1021 <panisan1979@gmail.com>
2026-03-11 04:35:00 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,52 +1,67 @@
 --[[ Holon VM v5 Secure ]]
-local _bkknh = {204,61,63,60,238,62,61,60,219,61,61,60,204,61,63,60,238,62,62,60,219,61,61,60,234,60,60,60,243,60,60,60}
-local _qexud = {{116,27,119,24,118,86,0,77,109,57,92,47,91,97,65,169,28,171,78,197,80,182,62,174,75,193,94},{216,99,205,43,168,27,248,123,229,6,132,51,208,83,224,4,188,54,213,84,243,22,184,39,207,110,226,1,128,21,246,116,248,27,154,60,223,94,218,57,184,6,229,100,253,211,253,211},{76,62,87,57,77}}
-local _gqsot = 60
-local function _zrbzg(...)
-    local _xwtbr, _lrbnm, _gingo = 1, {}, getfenv() or _G
-    local _qyqyg = {}
-    for i, v in ipairs(_qexud) do
+local _jnykn = {3,98,96,99,86,99,99,99,130,97,98,99,206,98,98,99,3,98,96,99,86,99,99,99,130,97,97,99,86,99,99,99,206,98,98,99,162,99,99,99}
+local _vjubc = {{43,68,40,71,41,9,95,18,50,102,3,112,4,62,30,246,67,244,17,154,15,233,97,241,20,158,1},{135,60,146,116,247,68,167,36,186,89,219,108,143,12,191,91,227,105,138,11,172,73,231,120,144,49,189,94,223,74,169,43,167,68,197,99,128,1,133,102,231,89,186,59,162,140,162,140},{19,97,8,102,18}}
+local _vosiu = 99
+local function _sveho(...)
+    local _tjjdp, _lifen, _ilwgc = 1, {}, getfenv() or _G
+    local _rpvwz = {}
+    for i, v in ipairs(_vjubc) do
         local s = ""
-        local last_byte = _gqsot -- 最初のキーはVMキー
+        local last_byte = _vosiu -- 最初のキーはVMキー
         for j = 1, #v do
             local enc_byte = v[j]
             local dec_byte = bit32.bxor(enc_byte, last_byte)
             s = s .. string.char(dec_byte)
             last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
         end
-        _qyqyg[i] = s
+        _rpvwz[i] = s
     end
 
-    local _xmokc = false
-    local _bdjua = {}
-    _bdjua[204] = function(_uiuzk) _lrbnm[_uiuzk[2]] = _gingo[_qyqyg[_uiuzk[3]]] end
-    _bdjua[238]     = function(_uiuzk) _lrbnm[_uiuzk[2]] = _qyqyg[_uiuzk[3]] end
-    _bdjua[219]      = function(_uiuzk) local f = _lrbnm[_uiuzk[2]]; if f then f(_lrbnm[_uiuzk[2]+1]) end end
-    _bdjua[243]    = function() _xmokc = true end
+    local _kcves = false
+    local _vnppm = {}
+    _vnppm[3] = function(_rgkff) _lifen[_rgkff[2]] = _ilwgc[_rpvwz[_rgkff[3]]] end
+    _vnppm[130]     = function(_rgkff) _lifen[_rgkff[2]] = _rpvwz[_rgkff[3]] end
+    _vnppm[206]      = function(_rgkff) local f = _lifen[_rgkff[2]]; if f then f(_lifen[_rgkff[2]+1]) end end
+    _vnppm[162]    = function() _kcves = true end
     
     -- NOOP & Math Ops
-    _bdjua[234]      = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] end
+    _vnppm[86]      = function(_rgkff) _lifen[_rgkff[2]] = _lifen[_rgkff[3]] end
     -- 算術演算命令 (R1 = R2 + R3)
-    _bdjua[106]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] + _lrbnm[_uiuzk[4]] end
-    _bdjua[183]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] - _lrbnm[_uiuzk[4]] end
-    _bdjua[198]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] * _lrbnm[_uiuzk[4]] end
-    _bdjua[20]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] / _lrbnm[_uiuzk[4]] end
+    _vnppm[110]       = function(_rgkff) _lifen[_rgkff[2]] = _lifen[_rgkff[3]] + _lifen[_rgkff[4]] end
+    _vnppm[251]       = function(_rgkff) _lifen[_rgkff[2]] = _lifen[_rgkff[3]] - _lifen[_rgkff[4]] end
+    _vnppm[13]       = function(_rgkff) _lifen[_rgkff[2]] = _lifen[_rgkff[3]] * _lifen[_rgkff[4]] end
+    _vnppm[43]       = function(_rgkff) _lifen[_rgkff[2]] = _lifen[_rgkff[3]] / _lifen[_rgkff[4]] end
 
-
-    local _oiqpj = 4 -- Instruction Width
-    while not _xmokc and _xwtbr <= #_bkknh do
-        local _podpa = _bkknh[_xwtbr]
-        local handler = _bdjua[_podpa]
-        if handler then
-            local _uiuzk = {
-                _podpa,
-                bit32.bxor(_bkknh[_xwtbr+1], _gqsot),
-                bit32.bxor(_bkknh[_xwtbr+2], _gqsot),
-                bit32.bxor(_bkknh[_xwtbr+3], _gqsot)
-            }
-            handler(_uiuzk)
+    local _iqrei = 4 -- Instruction Width
+    
+    -- 制御フロー平坦化 (Control Flow Flattening)
+    local _hsjcd = 217 -- _hsjcd: State, 初期状態はFETCH
+    local _iokov, _wcgrj -- _iokov: Temp Opcode, _wcgrj: Temp Instruction
+    
+    while _hsjcd ~= 101 do
+        if _hsjcd == 217 then
+            if not _kcves and _tjjdp <= #_jnykn then
+                _iokov = _jnykn[_tjjdp] -- オペコードを一時保存
+                _wcgrj = {      -- 命令全体を復号して一時保存
+                    _iokov,
+                    bit32.bxor(_jnykn[_tjjdp+1], _vosiu),
+                    bit32.bxor(_jnykn[_tjjdp+2], _vosiu),
+                    bit32.bxor(_jnykn[_tjjdp+3], _vosiu)
+                }
+                _hsjcd = 46 -- 次はDISPATCH状態へ
+            else
+                _hsjcd = 101 -- ループ終了
+            end
+        elseif _hsjcd == 46 then
+            local handler = _vnppm[_iokov]
+            if handler then
+                handler(_wcgrj) -- 保存しておいた命令を実行
+            end
+            _tjjdp = _tjjdp + _iqrei
+            _hsjcd = 217 -- 次はFETCH状態へ戻る
+        else
+            _hsjcd = 101 -- 不正な状態なら終了
         end
-        _xwtbr = _xwtbr + _oiqpj
     end
 end
-_zrbzg(...)
+_sveho(...)
