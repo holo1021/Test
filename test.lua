846d21a4703d28081d22cc936ab5d0424a6adc10
holo1021 <panisan1979@gmail.com>
2026-03-11 02:37:32 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,46 +1,52 @@
 --[[ Holon VM v5 Secure ]]
-local _yirve = {54,175,173,174,32,172,175,174,35,174,174,174,155,175,175,174,35,174,174,174,54,175,173,174,32,172,172,174,35,174,174,174,155,175,175,174,35,174,174,174,118,174,174,174}
-local _lqety = {{246,29,26,29,28,206,4,251,206,2,19,33,34,232,206,150,99,101,147,57,67,148,54,62,147,56,77},{146,105,92,148,49,97,145,49,76,145,48,101,145,49,97,146,102,56,145,47,85,147,92,77,150,79,58,145,47,67,145,48,58,145,47,84,145,47,50,145,47,108,145,47,71,220,220,220},{30,32,23,28,34}}
-local _gijrf = 174
-local function _xdnbd(...)
-    local _uecff, _pxyvr, _gtvmj = 1, {}, getfenv() or _G
-    local _dzqwj = {}
-    for i, v in ipairs(_lqety) do
+local _thryq = {192,196,198,197,252,199,196,197,47,196,196,197,192,196,198,197,252,199,199,197,47,196,196,197,5,197,197,197}
+local _hiekr = {{141,226,142,225,143,175,249,180,148,192,165,214,162,152,184,80,229,82,183,60,169,79,199,87,178,56,167},{33,154,52,210,81,226,1,130,28,255,125,202,41,170,25,253,69,207,44,173,10,239,65,222,54,151,27,248,121,236,15,141,1,226,99,197,38,167,35,192,65,255,28,157,4,42,4,42},{181,199,174,192,180}}
+local _rsnsk = 197
+local function _yzjfm(...)
+    local _ofboz, _fudyj, _wrcnd = 1, {}, getfenv() or _G
+    local _edyek = {}
+    for i, v in ipairs(_hiekr) do
         local s = ""
-        for j = 1, #v do s = s .. string.char((v[j] - _gijrf) % 256) end
-        _dzqwj[i] = s
+        local last_byte = _rsnsk -- 最初のキーはVMキー
+        for j = 1, #v do
+            local enc_byte = v[j]
+            local dec_byte = bit32.bxor(enc_byte, last_byte)
+            s = s .. string.char(dec_byte)
+            last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
+        end
+        _edyek[i] = s
     end
 
-    local _dmbhr = false
-    local _rmzpd = {}
-    _rmzpd[54] = function(_wltrf) _pxyvr[_wltrf[2]] = _gtvmj[_dzqwj[_wltrf[3]]] end
-    _rmzpd[32]     = function(_wltrf) _pxyvr[_wltrf[2]] = _dzqwj[_wltrf[3]] end
-    _rmzpd[155]      = function(_wltrf) local f = _pxyvr[_wltrf[2]]; if f then f(_pxyvr[_wltrf[2]+1]) end end
-    _rmzpd[118]    = function() _dmbhr = true end
+    local _rdmlj = false
+    local _kjmtu = {}
+    _kjmtu[192] = function(_ewodr) _fudyj[_ewodr[2]] = _wrcnd[_edyek[_ewodr[3]]] end
+    _kjmtu[252]     = function(_ewodr) _fudyj[_ewodr[2]] = _edyek[_ewodr[3]] end
+    _kjmtu[47]      = function(_ewodr) local f = _fudyj[_ewodr[2]]; if f then f(_fudyj[_ewodr[2]+1]) end end
+    _kjmtu[5]    = function() _rdmlj = true end
     
     -- NOOP & Math Ops
-    _rmzpd[35]      = function(_wltrf) _pxyvr[_wltrf[2]] = _pxyvr[_wltrf[3]] end
+    _kjmtu[37]      = function(_ewodr) _fudyj[_ewodr[2]] = _fudyj[_ewodr[3]] end
     -- 算術演算命令 (R1 = R2 + R3)
-    _rmzpd[233]       = function(_wltrf) _pxyvr[_wltrf[2]] = _pxyvr[_wltrf[3]] + _pxyvr[_wltrf[4]] end
-    _rmzpd[27]       = function(_wltrf) _pxyvr[_wltrf[2]] = _pxyvr[_wltrf[3]] - _pxyvr[_wltrf[4]] end
-    _rmzpd[107]       = function(_wltrf) _pxyvr[_wltrf[2]] = _pxyvr[_wltrf[3]] * _pxyvr[_wltrf[4]] end
-    _rmzpd[101]       = function(_wltrf) _pxyvr[_wltrf[2]] = _pxyvr[_wltrf[3]] / _pxyvr[_wltrf[4]] end
+    _kjmtu[168]       = function(_ewodr) _fudyj[_ewodr[2]] = _fudyj[_ewodr[3]] + _fudyj[_ewodr[4]] end
+    _kjmtu[214]       = function(_ewodr) _fudyj[_ewodr[2]] = _fudyj[_ewodr[3]] - _fudyj[_ewodr[4]] end
+    _kjmtu[174]       = function(_ewodr) _fudyj[_ewodr[2]] = _fudyj[_ewodr[3]] * _fudyj[_ewodr[4]] end
+    _kjmtu[43]       = function(_ewodr) _fudyj[_ewodr[2]] = _fudyj[_ewodr[3]] / _fudyj[_ewodr[4]] end
 
 
-    local _zkhhu = 4 -- Instruction Width
-    while not _dmbhr and _uecff <= #_yirve do
-        local _jvgwr = _yirve[_uecff]
-        local handler = _rmzpd[_jvgwr]
+    local _idosb = 4 -- Instruction Width
+    while not _rdmlj and _ofboz <= #_thryq do
+        local _ostzm = _thryq[_ofboz]
+        local handler = _kjmtu[_ostzm]
         if handler then
-            local _wltrf = {
-                _jvgwr,
-                bit32.bxor(_yirve[_uecff+1], _gijrf),
-                bit32.bxor(_yirve[_uecff+2], _gijrf),
-                bit32.bxor(_yirve[_uecff+3], _gijrf)
+            local _ewodr = {
+                _ostzm,
+                bit32.bxor(_thryq[_ofboz+1], _rsnsk),
+                bit32.bxor(_thryq[_ofboz+2], _rsnsk),
+                bit32.bxor(_thryq[_ofboz+3], _rsnsk)
             }
-            handler(_wltrf)
+            handler(_ewodr)
         end
-        _uecff = _uecff + _zkhhu
+        _ofboz = _ofboz + _idosb
     end
 end
-_xdnbd(...)
+_yzjfm(...)
