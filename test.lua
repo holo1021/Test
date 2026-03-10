2f645e9e2d485dc719eec55c20191f7598727596
holo1021 <panisan1979@gmail.com>
2026-03-11 03:39:28 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,52 +1,52 @@
 --[[ Holon VM v5 Secure ]]
-local _jrvod = {196,128,130,129,44,131,128,129,81,128,128,129,196,128,130,129,44,131,131,129,81,128,128,129,87,129,129,129}
-local _puaew = {{201,166,202,165,203,235,189,240,208,132,225,146,230,220,252,20,161,22,243,120,237,11,131,19,246,124,227},{101,222,112,150,21,166,69,198,88,187,57,142,109,238,93,185,1,139,104,233,78,171,5,154,114,211,95,188,61,168,75,201,69,166,39,129,98,227,103,132,5,187,88,217,64,110,64,110},{241,131,234,132,240}}
-local _jvihf = 129
-local function _zncud(...)
-    local _qhjrz, _llsdc, _qmhyu = 1, {}, getfenv() or _G
-    local _iygss = {}
-    for i, v in ipairs(_puaew) do
+local _bkknh = {204,61,63,60,238,62,61,60,219,61,61,60,204,61,63,60,238,62,62,60,219,61,61,60,234,60,60,60,243,60,60,60}
+local _qexud = {{116,27,119,24,118,86,0,77,109,57,92,47,91,97,65,169,28,171,78,197,80,182,62,174,75,193,94},{216,99,205,43,168,27,248,123,229,6,132,51,208,83,224,4,188,54,213,84,243,22,184,39,207,110,226,1,128,21,246,116,248,27,154,60,223,94,218,57,184,6,229,100,253,211,253,211},{76,62,87,57,77}}
+local _gqsot = 60
+local function _zrbzg(...)
+    local _xwtbr, _lrbnm, _gingo = 1, {}, getfenv() or _G
+    local _qyqyg = {}
+    for i, v in ipairs(_qexud) do
         local s = ""
-        local last_byte = _jvihf -- 最初のキーはVMキー
+        local last_byte = _gqsot -- 最初のキーはVMキー
         for j = 1, #v do
             local enc_byte = v[j]
             local dec_byte = bit32.bxor(enc_byte, last_byte)
             s = s .. string.char(dec_byte)
             last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
         end
-        _iygss[i] = s
+        _qyqyg[i] = s
     end
 
-    local _xxuzr = false
-    local _tiasu = {}
-    _tiasu[196] = function(_kjwkq) _llsdc[_kjwkq[2]] = _qmhyu[_iygss[_kjwkq[3]]] end
-    _tiasu[44]     = function(_kjwkq) _llsdc[_kjwkq[2]] = _iygss[_kjwkq[3]] end
-    _tiasu[81]      = function(_kjwkq) local f = _llsdc[_kjwkq[2]]; if f then f(_llsdc[_kjwkq[2]+1]) end end
-    _tiasu[87]    = function() _xxuzr = true end
+    local _xmokc = false
+    local _bdjua = {}
+    _bdjua[204] = function(_uiuzk) _lrbnm[_uiuzk[2]] = _gingo[_qyqyg[_uiuzk[3]]] end
+    _bdjua[238]     = function(_uiuzk) _lrbnm[_uiuzk[2]] = _qyqyg[_uiuzk[3]] end
+    _bdjua[219]      = function(_uiuzk) local f = _lrbnm[_uiuzk[2]]; if f then f(_lrbnm[_uiuzk[2]+1]) end end
+    _bdjua[243]    = function() _xmokc = true end
     
     -- NOOP & Math Ops
-    _tiasu[153]      = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] end
+    _bdjua[234]      = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] end
     -- 算術演算命令 (R1 = R2 + R3)
-    _tiasu[74]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] + _llsdc[_kjwkq[4]] end
-    _tiasu[72]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] - _llsdc[_kjwkq[4]] end
-    _tiasu[232]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] * _llsdc[_kjwkq[4]] end
-    _tiasu[193]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] / _llsdc[_kjwkq[4]] end
+    _bdjua[106]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] + _lrbnm[_uiuzk[4]] end
+    _bdjua[183]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] - _lrbnm[_uiuzk[4]] end
+    _bdjua[198]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] * _lrbnm[_uiuzk[4]] end
+    _bdjua[20]       = function(_uiuzk) _lrbnm[_uiuzk[2]] = _lrbnm[_uiuzk[3]] / _lrbnm[_uiuzk[4]] end
 
 
-    local _txuks = 4 -- Instruction Width
-    while not _xxuzr and _qhjrz <= #_jrvod do
-        local _sbxok = _jrvod[_qhjrz]
-        local handler = _tiasu[_sbxok]
+    local _oiqpj = 4 -- Instruction Width
+    while not _xmokc and _xwtbr <= #_bkknh do
+        local _podpa = _bkknh[_xwtbr]
+        local handler = _bdjua[_podpa]
         if handler then
-            local _kjwkq = {
-                _sbxok,
-                bit32.bxor(_jrvod[_qhjrz+1], _jvihf),
-                bit32.bxor(_jrvod[_qhjrz+2], _jvihf),
-                bit32.bxor(_jrvod[_qhjrz+3], _jvihf)
+            local _uiuzk = {
+                _podpa,
+                bit32.bxor(_bkknh[_xwtbr+1], _gqsot),
+                bit32.bxor(_bkknh[_xwtbr+2], _gqsot),
+                bit32.bxor(_bkknh[_xwtbr+3], _gqsot)
             }
-            handler(_kjwkq)
+            handler(_uiuzk)
         end
-        _qhjrz = _qhjrz + _txuks
+        _xwtbr = _xwtbr + _oiqpj
     end
 end
-_zncud(...)
+_zrbzg(...)
