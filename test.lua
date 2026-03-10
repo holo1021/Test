b246e3fca5a34744aa33317336a1e9c7af9c336d
holo1021 <panisan1979@gmail.com>
2026-03-11 03:09:06 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,72 +1,52 @@
 --[[ Holon VM v5 Secure ]]
-if not bit32 then
-    bit32 = {}
-    function bit32.bxor(a, b)
-        local r, p = 0, 1
-        while a > 0 or b > 0 do
-            local ra, rb = a % 2, b % 2
-            if ra ~= rb then r = r + p end
-            a, b = (a - ra) / 2, (b - rb) / 2
-            p = p * 2
-        end
-        return r
-    end
-end
-local _bzyte = {1,197,199,196,206,198,197,196,232,197,197,196,90,196,196,196,1,197,199,196,206,198,198,196,90,196,196,196,232,197,197,196,31,196,196,196}
-local _kktdp = {{140,227,143,224,142,174,248,181,149,193,164,215,163,153,185,81,228,83,182,61,168,78,198,86,179,57,166},{32,155,53,211,80,227,0,131,29,254,124,203,40,171,24,252,68,206,45,172,11,238,64,223,55,150,26,249,120,237,14,140,0,227,98,196,39,166,34,193,64,254,29,156,5,43,5,43},{180,198,175,193,181}}
-local _EH = {[134] = {155,200,147,204,133,222,236,177,236,204,241,209,142,221,134,217,144,203,248,165,248,216,245,213,138,217,130,221,148,207,251,166,251},[1] = {155,200,147,204,133,222,236,177,236,204,241,209,142,203,144,207,139,200,147,204,133,222,237,176,237,176},[232] = {168,199,164,197,169,137,239,207,242,210,141,222,133,218,147,200,250,167,250,193,225,136,238,206,168,136,252,148,241,159,191,217,241,174,253,166,249,176,235,217,132,175,158,195,234,202,175,193,165},[125] = {155,200,147,204,133,222,236,177,236,204,241,209,142,221,134,217,144,203,248,165,248,216,242,210,141,222,133,218,147,200,252,161,252},[9] = {155,200,147,204,133,222,236,177,236,204,241,209,142,221,134,217,144,203,248,165,248,216,247,215,136,219,128,223,150,205,249,164,249},[206] = {155,200,147,204,133,222,236,177,236,204,241,209,142,202,137,210,141,196,159,172,241,172},[150] = {155,200,147,204,133,222,236,177,236,204,241,209,142,221,134,217,144,203,248,165,248,216,243,211,140,223,132,219,146,201,253,160,253},[31] = {155,201,231,145,240,156,188,129,161,213,167,210,183},[90] = {155,200,147,204,133,222,236,177,236,204,241,209,142,221,134,217,144,203,248,165,248}}
-local _aeder = 196
-local function _yzajt(...)
-    local _fhwyv, _pcihy, _vbwtq = 1, {}, getfenv() or _G
-    local _dytii = {}
-    for i, v in ipairs(_kktdp) do
+local _jrvod = {196,128,130,129,44,131,128,129,81,128,128,129,196,128,130,129,44,131,131,129,81,128,128,129,87,129,129,129}
+local _puaew = {{201,166,202,165,203,235,189,240,208,132,225,146,230,220,252,20,161,22,243,120,237,11,131,19,246,124,227},{101,222,112,150,21,166,69,198,88,187,57,142,109,238,93,185,1,139,104,233,78,171,5,154,114,211,95,188,61,168,75,201,69,166,39,129,98,227,103,132,5,187,88,217,64,110,64,110},{241,131,234,132,240}}
+local _jvihf = 129
+local function _zncud(...)
+    local _qhjrz, _llsdc, _qmhyu = 1, {}, getfenv() or _G
+    local _iygss = {}
+    for i, v in ipairs(_puaew) do
         local s = ""
-        local last_byte = _aeder -- 最初のキーはVMキー
+        local last_byte = _jvihf -- 最初のキーはVMキー
         for j = 1, #v do
             local enc_byte = v[j]
             local dec_byte = bit32.bxor(enc_byte, last_byte)
             s = s .. string.char(dec_byte)
             last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
         end
-        _dytii[i] = s
+        _iygss[i] = s
     end
 
-    local _acosp = {val = false}
-    local _pdsuv = {}
-    local _LS = loadstring or load
+    local _xxuzr = false
+    local _tiasu = {}
+    _tiasu[196] = function(_kjwkq) _llsdc[_kjwkq[2]] = _qmhyu[_iygss[_kjwkq[3]]] end
+    _tiasu[44]     = function(_kjwkq) _llsdc[_kjwkq[2]] = _iygss[_kjwkq[3]] end
+    _tiasu[81]      = function(_kjwkq) local f = _llsdc[_kjwkq[2]]; if f then f(_llsdc[_kjwkq[2]+1]) end end
+    _tiasu[87]    = function() _xxuzr = true end
+    
+    -- NOOP & Math Ops
+    _tiasu[153]      = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] end
+    -- 算術演算命令 (R1 = R2 + R3)
+    _tiasu[74]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] + _llsdc[_kjwkq[4]] end
+    _tiasu[72]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] - _llsdc[_kjwkq[4]] end
+    _tiasu[232]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] * _llsdc[_kjwkq[4]] end
+    _tiasu[193]       = function(_kjwkq) _llsdc[_kjwkq[2]] = _llsdc[_kjwkq[3]] / _llsdc[_kjwkq[4]] end
 
-    for op, enc_body in pairs(_EH) do
-        local body_str = ""
-        local last_byte = _aeder
-        for j = 1, #enc_body do
-            local enc_byte = enc_body[j]
-            local dec_byte = bit32.bxor(enc_byte, last_byte)
-            body_str = body_str .. string.char(dec_byte)
-            last_byte = enc_byte
-        end
-        -- 引数として _pcihy, _dytii, _vbwtq, _acosp, _hrfhp を受け取るように変更
-        local chunk, err = _LS("return function(_pcihy, _dytii, _vbwtq, _acosp, _hrfhp) " .. body_str .. " end")
-        if chunk then
-            local handler = chunk() -- Execute the chunk to get the actual handler function
-            _pdsuv[op] = handler
-        end
-    end
 
-    local _ymkvq = 4
-    while not _acosp.val and _fhwyv <= #_bzyte do
-        local _gpolv = _bzyte[_fhwyv]
-        local handler = _pdsuv[_gpolv]
+    local _txuks = 4 -- Instruction Width
+    while not _xxuzr and _qhjrz <= #_jrvod do
+        local _sbxok = _jrvod[_qhjrz]
+        local handler = _tiasu[_sbxok]
         if handler then
-            local _hrfhp = {
-                _gpolv,
-                bit32.bxor(_bzyte[_fhwyv+1], _aeder),
-                bit32.bxor(_bzyte[_fhwyv+2], _aeder),
-                bit32.bxor(_bzyte[_fhwyv+3], _aeder)
+            local _kjwkq = {
+                _sbxok,
+                bit32.bxor(_jrvod[_qhjrz+1], _jvihf),
+                bit32.bxor(_jrvod[_qhjrz+2], _jvihf),
+                bit32.bxor(_jrvod[_qhjrz+3], _jvihf)
             }
-            -- 必要な変数をすべて引数として渡す
-            handler(_pcihy, _dytii, _vbwtq, _acosp, _hrfhp)
+            handler(_kjwkq)
         end
-        _fhwyv = _fhwyv + _ymkvq
+        _qhjrz = _qhjrz + _txuks
     end
 end
-_yzajt(...)
+_zncud(...)
