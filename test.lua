6d651c3c3b80c4f6c6613ba6ee5b4d7d7850253a
holo1021 <panisan1979@gmail.com>
2026-03-11 22:57:10 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,20 @@
 --[[ Holon VM v5 Secure ]]
-local _rkcob = {35,60,61,60,119,61,62,60,232,60,62,61,35,62,61,60,119,63,63,60,232,62,62,61,167,60,61,60}
-local _jpxri = {{76,62,87,57,77},{116,27,119,24,118,86,0,77,109,57,92,47,91,97,65,169,28,171,78,197,80,182,62,174,75,193,94},{216,99,205,43,168,27,248,123,229,6,132,51,208,83,224,4,188,54,213,84,243,22,184,39,207,110,226,1,128,21,246,116,248,27,154,60,223,94,218,57,184,6,229,100,253,211,253,211}}
-local _nrgpg = {}
-local _wdzuc = 60
-local function _nwmod(...)
-    local _ddxsj, _khgep = {}, getfenv() or _G
-    local _cvttg = {} -- Call Stack: {cl, pc, base, ret_reg}
+local _adsrf = {206,124,125,124,39,125,126,124,136,124,126,125,206,126,125,124,39,127,127,124,136,126,126,125,35,124,125,124}
+local _xkccq = {{12,126,23,121,13},{52,91,55,88,54,22,64,13,45,121,28,111,27,33,1,233,92,235,14,133,16,246,126,238,11,129,30},{152,35,141,107,232,91,184,59,165,70,196,115,144,19,160,68,252,118,149,20,179,86,248,103,143,46,162,65,192,85,182,52,184,91,218,124,159,30,154,121,248,70,165,36,189,147,189,147}}
+local _yipkn = {}
+local _zmgfn = 124
+local function _czbvw(...)
+    local _savos, _wwage = {}, getfenv() or _G
+    local _bgcjm = {} -- Call Stack: {cl, pc, base, ret_reg}
 
     -- Helper to decode constants for a prototype, with caching
-    local _jvfte = setmetatable({}, {__mode = "k"})
-    local function _hsfvx(p)
-        if _jvfte[p] then return _jvfte[p] end
+    local _hxgye = setmetatable({}, {__mode = "k"})
+    local function _oinjz(p)
+        if _hxgye[p] then return _hxgye[p] end
         local dc = {}
         for i, v in ipairs(p.c) do
             local s = ""
-            local last_byte = _wdzuc
+            local last_byte = _zmgfn
             for j = 1, #v do
                 local enc_byte = v[j]
                 local dec_byte = bit32.bxor(enc_byte, last_byte)
@@ -23,78 +23,75 @@ local function _nwmod(...)
             end
             dc[i] = s
         end
-        _jvfte[p] = dc
+        _hxgye[p] = dc
         return dc
     end
 
     -- Main closure setup
-    local _CL = { p = { b = _rkcob, c = _jpxri, p = _nrgpg, np = 0 } }
-    table.insert(_cvttg, { cl = _CL, pc = 1, base = 1, rr = 1 })
+    local _CL = { p = { b = _adsrf, c = _xkccq, p = _yipkn, np = 0 } }
+    table.insert(_bgcjm, { cl = _CL, pc = 1, base = 1, rr = 1 })
 
     -- VM命令ハンドラテーブル
-    local _ejuhs = {
-        [209] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] end,
-        [119] = function(_I, _ddxsj, _BASE, _DC) _ddxsj[_BASE + _I[2]] = _DC[_I[3]] end,
-        [35] = function(_I, _ddxsj, _BASE, _DC, _khgep) _ddxsj[_BASE + _I[2]] = _khgep[_DC[_I[3]]] end,
-        [22] = function(_I, _ddxsj, _BASE, _DC, _khgep) _khgep[_DC[_I[3]]] = _ddxsj[_BASE + _I[2]] end,
-        [69] = function(_I, _ddxsj, _BASE, _DC, _khgep, _CL) _ddxsj[_BASE + _I[2]] = { p = _CL.p.p[_I[3]], uv = {} } end,
-        [232] = function(_I, _ddxsj, _BASE, _DC, _khgep, _CL, _cvttg)
+    local _aalor = {
+        [239] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] end,
+        [39] = function(_I, _savos, _BASE, _DC) _savos[_BASE + _I[2]] = _DC[_I[3]] end,
+        [206] = function(_I, _savos, _BASE, _DC, _wwage) _savos[_BASE + _I[2]] = _wwage[_DC[_I[3]]] end,
+        [184] = function(_I, _savos, _BASE, _DC, _wwage) _wwage[_DC[_I[3]]] = _savos[_BASE + _I[2]] end,
+        [19] = function(_I, _savos, _BASE, _DC, _wwage, _CL) _savos[_BASE + _I[2]] = { p = _CL.p.p[_I[3]], uv = {} } end,
+        [136] = function(_I, _savos, _BASE, _DC, _wwage, _CL, _bgcjm)
             local func_reg, n_args, n_ret = _I[2], _I[3] - 1, _I[4] - 1
-            local func = _ddxsj[_BASE + func_reg]
-
-            if not func then
-                error("VM Error: Attempt to call a nil value at register " .. func_reg)
-            elseif type(func) == "function" then
-                local args = {}
-                for i = 1, n_args do table.insert(args, _ddxsj[_BASE + func_reg + i]) end
-                local results = {pcall(func, unpack(args))}
-                for i = 1, n_ret do _ddxsj[_BASE + func_reg + i - 1] = results[i+1] end
-            elseif type(func) == "table" and func.p then -- Holon Closure
-                table.insert(_cvttg, { cl = func, pc = 1, base = _BASE + func_reg, rr = _BASE + func_reg })
-            else
-                error("VM Error: Attempt to call a non-function/non-closure value (type: " .. type(func) .. ") at register " .. func_reg)
+            local func = _savos[_BASE + func_reg]
+            if func then
+                if type(func) == "function" then
+                    local args = {}
+                    for i = 1, n_args do args[i] = _savos[_BASE + func_reg + i] end
+                    -- pcallを削除し、直接呼び出すことでエラーを隠蔽せず、unpackの範囲を明示してnil安全性を高めます
+                    local results = {func(unpack(args, 1, n_args))}
+                    for i = 1, n_ret do _savos[_BASE + func_reg + i - 1] = results[i+1] end
+                else
+                    table.insert(_bgcjm, { cl = func, pc = 1, base = _BASE + func_reg, rr = _BASE + func_reg })
+                end
             end
         end,
-
-        [167] = function(_I, _ddxsj, _BASE, _DC, _khgep, _CL, _cvttg, frame)
+        [35] = function(_I, _savos, _BASE, _DC, _wwage, _CL, _bgcjm, frame)
             local ret_start, n_ret = _I[2], _I[3] - 1
-            if #_cvttg > 1 then -- 呼び出し元がある場合のみ戻り値を設定
-                for i = 1, n_ret do _ddxsj[frame.rr + i - 1] = _ddxsj[_BASE + ret_start + i - 1] end
+            if #_bgcjm > 1 then -- 呼び出し元がある場合のみ戻り値を設定
+                for i = 1, n_ret do _savos[frame.rr + i - 1] = _savos[_BASE + ret_start + i - 1] end
             end
-            table.remove(_cvttg)
+            table.remove(_bgcjm)
         end,
-        [155] = function(_I, _ddxsj, _BASE, _DC, _khgep, _CL, _cvttg, frame, _P, _qmzdr) frame.pc = _P + _I[4] * _qmzdr end,
-        [120] = function(_I, _ddxsj, _BASE, _DC, _khgep, _CL, _cvttg, frame, _P, _qmzdr) if not _ddxsj[_BASE + _I[2]] then frame.pc = _P + _I[4] * _qmzdr end end,
-        [128] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] == _ddxsj[_BASE + _I[4]] end,
-        [173] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] + _ddxsj[_BASE + _I[4]] end,
-        [177] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] - _ddxsj[_BASE + _I[4]] end,
-        [182] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] * _ddxsj[_BASE + _I[4]] end,
-        [153] = function(_I, _ddxsj, _BASE) _ddxsj[_BASE + _I[2]] = _ddxsj[_BASE + _I[3]] / _ddxsj[_BASE + _I[4]] end,
-        [250] = function() end -- 何もしない
+        [159] = function(_I, _savos, _BASE, _DC, _wwage, _CL, _bgcjm, frame, _P, _ipwgg) frame.pc = _P + _I[4] * _ipwgg end,
+        [247] = function(_I, _savos, _BASE, _DC, _wwage, _CL, _bgcjm, frame, _P, _ipwgg) if not _savos[_BASE + _I[2]] then frame.pc = _P + _I[4] * _ipwgg end end,
+        [98] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] == _savos[_BASE + _I[4]] end,
+        [212] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] + _savos[_BASE + _I[4]] end,
+        [153] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] - _savos[_BASE + _I[4]] end,
+        [224] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] * _savos[_BASE + _I[4]] end,
+        [203] = function(_I, _savos, _BASE) _savos[_BASE + _I[2]] = _savos[_BASE + _I[3]] / _savos[_BASE + _I[4]] end,
+        [71] = function() end -- 何もしない
     }
 
-    local _qmzdr = 4 -- Instruction Width
+    local _ipwgg = 4 -- Instruction Width
 
-    while #_cvttg > 0 do
-        local frame = _cvttg[#_cvttg]
+    while #_bgcjm > 0 do
+        local frame = _bgcjm[#_bgcjm]
         local _CL, _P, _BASE = frame.cl, frame.pc, frame.base
-        if not _CL then table.remove(_cvttg); goto continue_loop end -- 安全策: 不正なコールスタックを削除
+        if not _CL then table.remove(_bgcjm); goto continue_loop end -- 安全策: 不正なコールスタックを削除
         local _BC = _CL.p.b
-        local _DC = _hsfvx(_CL.p)
+        local _DC = _oinjz(_CL.p)
 
         if _P <= #_BC then
             local _O = _BC[_P]
-            frame.pc = _P + _qmzdr
-            local handler = _ejuhs[_O]
+            frame.pc = _P + _ipwgg
+            local handler = _aalor[_O]
             if handler then
-                local _I = { _O, bit32.bxor(_BC[_P+1], _wdzuc), bit32.bxor(_BC[_P+2], _wdzuc), bit32.bxor(_BC[_P+3], _wdzuc) }
-                handler(_I, _ddxsj, _BASE, _DC, _khgep, _CL, _cvttg, frame, _P, _qmzdr)
+                local _I = { _O, bit32.bxor(_BC[_P+1], _zmgfn), bit32.bxor(_BC[_P+2], _zmgfn), bit32.bxor(_BC[_P+3], _zmgfn) }
+                handler(_I, _savos, _BASE, _DC, _wwage, _CL, _bgcjm, frame, _P, _ipwgg)
             end
         else
-            table.remove(_cvttg) -- バイトコードの終端に達したら暗黙的にリターン
+            table.remove(_bgcjm) -- バイトコードの終端に達したら暗黙的にリターン
         end
 
         ::continue_loop::
     end
 end
-_nwmod(...)
+_czbvw(...)
