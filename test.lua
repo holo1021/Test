3766e6da3fe7f4f8d28380d4dea1d6307cb0083e
holo1021 <panisan1979@gmail.com>
2026-03-11 21:22:01 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,64 +1,64 @@
 --[[ Holon VM v5 Secure ]]
-local _swqhb = {143,92,93,92,138,93,94,92,171,92,93,92,143,94,93,92,138,95,95,92,171,94,93,92,191,92,92,92}
-local _ixjqh = {{44,94,55,89,45},{20,123,23,120,22,54,96,45,13,89,60,79,59,1,33,201,124,203,46,165,48,214,94,206,43,161,62},{184,3,173,75,200,123,152,27,133,102,228,83,176,51,128,100,220,86,181,52,147,118,216,71,175,14,130,97,224,117,150,20,152,123,250,92,191,62,186,89,216,102,133,4,157,179,157,179}}
-local _mvslf = 92
-local function _pegqi(...)
-    local _yodxt, _sncxo, _pcivz = 1, {}, getfenv() or _G
-    local _ettgn = {}
-    for i, v in ipairs(_ixjqh) do
+local _zbdmz = {184,14,15,14,155,15,12,14,83,14,15,14,184,12,15,14,155,13,13,14,83,12,15,14,229,14,14,14}
+local _wwawe = {{126,12,101,11,127},{70,41,69,42,68,100,50,127,95,11,110,29,105,83,115,155,46,153,124,247,98,132,12,156,121,243,108},{234,81,255,25,154,41,202,73,215,52,182,1,226,97,210,54,142,4,231,102,193,36,138,21,253,92,208,51,178,39,196,70,202,41,168,14,237,108,232,11,138,52,215,86,207,225,207,225}}
+local _pfikd = 14
+local function _gzvjc(...)
+    local _xwjbr, _fpifu, _zpdrd = 1, {}, getfenv() or _G
+    local _khrty = {}
+    for i, v in ipairs(_wwawe) do
         local s = ""
-        local last_byte = _mvslf -- 最初のキーはVMキー
+        local last_byte = _pfikd -- 最初のキーはVMキー
         for j = 1, #v do
             local enc_byte = v[j]
             local dec_byte = bit32.bxor(enc_byte, last_byte)
             s = s .. string.char(dec_byte)
             last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
         end
-        _ettgn[i] = s
+        _khrty[i] = s
     end
 
-    local _xhyiv = false
-    local _akeuj = {}
-    _akeuj[152]      = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] end
-    _akeuj[143] = function(_ltuvp) _sncxo[_ltuvp[2]] = _pcivz[_ettgn[_ltuvp[3]]] end
-    _akeuj[138]     = function(_ltuvp) _sncxo[_ltuvp[2]] = _ettgn[_ltuvp[3]] end
-    _akeuj[171]      = function(_ltuvp)
-        local f = _sncxo[_ltuvp[2]]
+    local _eydlf = false
+    local _msfue = {}
+    _msfue[207]      = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] end
+    _msfue[184] = function(_ropvy) _fpifu[_ropvy[2]] = _zpdrd[_khrty[_ropvy[3]]] end
+    _msfue[155]     = function(_ropvy) _fpifu[_ropvy[2]] = _khrty[_ropvy[3]] end
+    _msfue[83]      = function(_ropvy)
+        local f = _fpifu[_ropvy[2]]
         if not f then return end
-        local nargs = _ltuvp[3]
+        local nargs = _ropvy[3]
         local args = {}
         for i = 1, nargs do
-            table.insert(args, _sncxo[_ltuvp[2] + i])
+            table.insert(args, _fpifu[_ropvy[2] + i])
         end
         f(unpack(args))
     end
-    _akeuj[191]    = function() _xhyiv = true end
+    _msfue[229]    = function() _eydlf = true end
     
     -- Control Flow, NOOP & Math Ops
-    _akeuj[99]       = function(_ltuvp) _yodxt = _yodxt + _ltuvp[4] * _aubav end
-    _akeuj[98]      = function(_ltuvp) if not _sncxo[_ltuvp[2]] then _yodxt = _yodxt + _ltuvp[4] * _aubav end end
-    _akeuj[51]        = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] == _sncxo[_ltuvp[4]] end
-    _akeuj[247]      = function() end -- 何もしない
-    _akeuj[254]       = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] + _sncxo[_ltuvp[4]] end
-    _akeuj[173]       = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] - _sncxo[_ltuvp[4]] end
-    _akeuj[63]       = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] * _sncxo[_ltuvp[4]] end
-    _akeuj[224]       = function(_ltuvp) _sncxo[_ltuvp[2]] = _sncxo[_ltuvp[3]] / _sncxo[_ltuvp[4]] end
+    _msfue[54]       = function(_ropvy) _xwjbr = _xwjbr + _ropvy[4] * _vgzoz end
+    _msfue[242]      = function(_ropvy) if not _fpifu[_ropvy[2]] then _xwjbr = _xwjbr + _ropvy[4] * _vgzoz end end
+    _msfue[214]        = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] == _fpifu[_ropvy[4]] end
+    _msfue[221]      = function() end -- 何もしない
+    _msfue[165]       = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] + _fpifu[_ropvy[4]] end
+    _msfue[220]       = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] - _fpifu[_ropvy[4]] end
+    _msfue[190]       = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] * _fpifu[_ropvy[4]] end
+    _msfue[21]       = function(_ropvy) _fpifu[_ropvy[2]] = _fpifu[_ropvy[3]] / _fpifu[_ropvy[4]] end
 
 
-    local _aubav = 4 -- Instruction Width
-    while not _xhyiv and _yodxt <= #_swqhb do
-        local _agsdg = _swqhb[_yodxt]
-        local handler = _akeuj[_agsdg]
+    local _vgzoz = 4 -- Instruction Width
+    while not _eydlf and _xwjbr <= #_zbdmz do
+        local _uqocy = _zbdmz[_xwjbr]
+        local handler = _msfue[_uqocy]
         if handler then
-            local _ltuvp = {
-                _agsdg,
-                bit32.bxor(_swqhb[_yodxt+1], _mvslf),
-                bit32.bxor(_swqhb[_yodxt+2], _mvslf),
-                bit32.bxor(_swqhb[_yodxt+3], _mvslf)
+            local _ropvy = {
+                _uqocy,
+                bit32.bxor(_zbdmz[_xwjbr+1], _pfikd),
+                bit32.bxor(_zbdmz[_xwjbr+2], _pfikd),
+                bit32.bxor(_zbdmz[_xwjbr+3], _pfikd)
             }
-            handler(_ltuvp)
+            handler(_ropvy)
         end
-        _yodxt = _yodxt + _aubav
+        _xwjbr = _xwjbr + _vgzoz
     end
 end
-_pegqi(...)
+_gzvjc(...)
