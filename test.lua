332d3fb85faa549b8bea22d24a52420e21041415
holo1021 <panisan1979@gmail.com>
2026-03-11 22:18:33 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,20 @@
 --[[ Holon VM v5 Secure ]]
-local _vuxxx = {149,128,129,128,139,129,130,128,116,128,130,129,149,130,129,128,139,131,131,128,116,130,130,129,7,128,129,128}
-local _ryzdh = {{240,130,235,133,241},{200,167,203,164,202,234,188,241,209,133,224,147,231,221,253,21,160,23,242,121,236,10,130,18,247,125,226},{100,223,113,151,20,167,68,199,89,186,56,143,108,239,92,184,0,138,105,232,79,170,4,155,115,210,94,189,60,169,74,200,68,167,38,128,99,226,102,133,4,186,89,216,65,111,65,111}}
-local _PROTOS = {}
-local _cvbsv = 128
-local function _dubjr(...)
-    local _yepzo, _oeape = {}, getfenv() or _G
-    local _CS = {} -- Call Stack: {cl, pc, base, ret_reg}
+local _qnsfr = {237,50,51,50,31,51,48,50,106,50,48,51,237,48,51,50,31,49,49,50,106,48,48,51,211,50,51,50}
+local _nujxv = {{66,48,89,55,67},{122,21,121,22,120,88,14,67,99,55,82,33,85,111,79,167,18,165,64,203,94,184,48,160,69,207,80},{214,109,195,37,166,21,246,117,235,8,138,61,222,93,238,10,178,56,219,90,253,24,182,41,193,96,236,15,142,27,248,122,246,21,148,50,209,80,212,55,182,8,235,106,243,221,243,221}}
+local _yxinf = {}
+local _mlpbu = 50
+local function _ovcrj(...)
+    local _bvkbg, _igysp = {}, getfenv() or _G
+    local _auqcn = {} -- Call Stack: {cl, pc, base, ret_reg}
 
     -- Helper to decode constants for a prototype, with caching
-    local _DCCache = setmetatable({}, {__mode = "k"})
-    local function get_dc(p)
-        if _DCCache[p] then return _DCCache[p] end
+    local _tqchk = setmetatable({}, {__mode = "k"})
+    local function _nvqiv(p)
+        if _tqchk[p] then return _tqchk[p] end
         local dc = {}
         for i, v in ipairs(p.c) do
             local s = ""
-            local last_byte = _cvbsv
+            local last_byte = _mlpbu
             for j = 1, #v do
                 local enc_byte = v[j]
                 local dec_byte = bit32.bxor(enc_byte, last_byte)
@@ -23,67 +23,74 @@ local function _dubjr(...)
             end
             dc[i] = s
         end
-        _DCCache[p] = dc
+        _tqchk[p] = dc
         return dc
     end
 
     -- Main closure setup
-    local _CL = { p = { b = _vuxxx, c = _ryzdh, p = _PROTOS, np = 0 } }
-    table.insert(_CS, { cl = _CL, pc = 1, base = 1, rr = 1 })
+    local _CL = { p = { b = _qnsfr, c = _nujxv, p = _yxinf, np = 0 } }
+    table.insert(_auqcn, { cl = _CL, pc = 1, base = 1, rr = 1 })
 
-    local _drwob = {}
-    local _anofx = 4 -- Instruction Width
+    -- VM命令ハンドラテーブル
+    local _ndqjj = {
+        [45] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] end,
+        [31] = function(_I, _bvkbg, _BASE, _DC) _bvkbg[_BASE + _I[2]] = _DC[_I[3]] end,
+        [237] = function(_I, _bvkbg, _BASE, _DC, _igysp) _bvkbg[_BASE + _I[2]] = _igysp[_DC[_I[3]]] end,
+        [212] = function(_I, _bvkbg, _BASE, _DC, _igysp) _igysp[_DC[_I[3]]] = _bvkbg[_BASE + _I[2]] end,
+        [235] = function(_I, _bvkbg, _BASE, _DC, _igysp, _CL) _bvkbg[_BASE + _I[2]] = { p = _CL.p.p[_I[3]], uv = {} } end,
+        [106] = function(_I, _bvkbg, _BASE, _DC, _igysp, _CL, _auqcn)
+            local func_reg, n_args, n_ret = _I[2], _I[3] - 1, _I[4] - 1
+            local func = _bvkbg[_BASE + func_reg]
+            if func then
+                if type(func) == "function" then
+                    local args = {}
+                    for i = 1, n_args do table.insert(args, _bvkbg[_BASE + func_reg + i]) end
+                    local results = {pcall(func, unpack(args))}
+                    for i = 1, n_ret do _bvkbg[_BASE + func_reg + i - 1] = results[i+1] end
+                else
+                    table.insert(_auqcn, { cl = func, pc = 1, base = _BASE + func_reg, rr = _BASE + func_reg })
+                end
+            end
+        end,
+        [211] = function(_I, _bvkbg, _BASE, _DC, _igysp, _CL, _auqcn, frame)
+            local ret_start, n_ret = _I[2], _I[3] - 1
+            if #_auqcn > 1 then -- 呼び出し元がある場合のみ戻り値を設定
+                for i = 1, n_ret do _bvkbg[frame.rr + i - 1] = _bvkbg[_BASE + ret_start + i - 1] end
+            end
+            table.remove(_auqcn)
+        end,
+        [11] = function(_I, _bvkbg, _BASE, _DC, _igysp, _CL, _auqcn, frame, _P, _eobyt) frame.pc = _P + _I[4] * _eobyt end,
+        [210] = function(_I, _bvkbg, _BASE, _DC, _igysp, _CL, _auqcn, frame, _P, _eobyt) if not _bvkbg[_BASE + _I[2]] then frame.pc = _P + _I[4] * _eobyt end end,
+        [189] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] == _bvkbg[_BASE + _I[4]] end,
+        [176] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] + _bvkbg[_BASE + _I[4]] end,
+        [254] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] - _bvkbg[_BASE + _I[4]] end,
+        [253] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] * _bvkbg[_BASE + _I[4]] end,
+        [125] = function(_I, _bvkbg, _BASE) _bvkbg[_BASE + _I[2]] = _bvkbg[_BASE + _I[3]] / _bvkbg[_BASE + _I[4]] end,
+        [14] = function() end -- 何もしない
+    }
 
-    while #_CS > 0 do
-        local frame = _CS[#_CS]
-        local _CL, _vsydu, _BASE = frame.cl, frame.pc, frame.base
+    local _eobyt = 4 -- Instruction Width
+
+    while #_auqcn > 0 do
+        local frame = _auqcn[#_auqcn]
+        local _CL, _P, _BASE = frame.cl, frame.pc, frame.base
+        if not _CL then table.remove(_auqcn); goto continue_loop end -- 安全策: 不正なコールスタックを削除
         local _BC = _CL.p.b
-        local _iqxll = get_dc(_CL.p)
+        local _DC = _nvqiv(_CL.p)
 
-        if _vsydu > #_BC then
-            -- Implicit return from function
-            table.remove(_CS)
-            goto continue_loop
-        end
-
-        local _nanby = _BC[_vsydu]
-        frame.pc = _vsydu + _anofx
-
-        local _iifes = { _nanby, bit32.bxor(_BC[_vsydu+1], _cvbsv), bit32.bxor(_BC[_vsydu+2], _cvbsv), bit32.bxor(_BC[_vsydu+3], _cvbsv) }
-
-        if _nanby == 247 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]]
-        elseif _nanby == 139 then _yepzo[_BASE + _iifes[2]] = _iqxll[_iifes[3]]
-        elseif _nanby == 149 then _yepzo[_BASE + _iifes[2]] = _oeape[_iqxll[_iifes[3]]]
-        elseif _nanby == 5 then _oeape[_iqxll[_iifes[3]]] = _yepzo[_BASE + _iifes[2]]
-        elseif _nanby == 207 then _yepzo[_BASE + _iifes[2]] = { p = _CL.p.p[_iifes[3]], uv = {} }
-        elseif _nanby == 116 then
-            local func_reg, n_args, n_ret = _iifes[2], _iifes[3] - 1, _iifes[4] - 1
-            local func = _yepzo[_BASE + func_reg]
-            if type(func) == "function" then
-                local args = {}
-                for i = 1, n_args do table.insert(args, _yepzo[_BASE + func_reg + i]) end
-                local results = {pcall(func, unpack(args))}
-                for i = 1, n_ret do _yepzo[_BASE + func_reg + i - 1] = results[i+1] end
-            else
-                table.insert(_CS, { cl = func, pc = 1, base = _BASE + func_reg, rr = _BASE + func_reg })
+        if _P <= #_BC then
+            local _O = _BC[_P]
+            frame.pc = _P + _eobyt
+            local handler = _ndqjj[_O]
+            if handler then
+                local _I = { _O, bit32.bxor(_BC[_P+1], _mlpbu), bit32.bxor(_BC[_P+2], _mlpbu), bit32.bxor(_BC[_P+3], _mlpbu) }
+                handler(_I, _bvkbg, _BASE, _DC, _igysp, _CL, _auqcn, frame, _P, _eobyt)
             end
-        elseif _nanby == 7 then
-            local ret_start, n_ret = _iifes[2], _iifes[3] - 1
-            local caller_frame = _CS[#_CS-1]
-            if caller_frame then
-                for i = 1, n_ret do _yepzo[frame.rr + i - 1] = _yepzo[_BASE + ret_start + i - 1] end
-            end
-            table.remove(_CS)
-        elseif _nanby == 255 then frame.pc = _vsydu + _iifes[4] * _anofx
-        elseif _nanby == 29 then if not _yepzo[_BASE + _iifes[2]] then frame.pc = _vsydu + _iifes[4] * _anofx end
-        elseif _nanby == 60 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]] == _yepzo[_BASE + _iifes[4]]
-        elseif _nanby == 64 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]] + _yepzo[_BASE + _iifes[4]]
-        elseif _nanby == 78 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]] - _yepzo[_BASE + _iifes[4]]
-        elseif _nanby == 173 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]] * _yepzo[_BASE + _iifes[4]]
-        elseif _nanby == 164 then _yepzo[_BASE + _iifes[2]] = _yepzo[_BASE + _iifes[3]] / _yepzo[_BASE + _iifes[4]]
+        else
+            table.remove(_auqcn) -- バイトコードの終端に達したら暗黙的にリターン
         end
 
         ::continue_loop::
     end
 end
-_dubjr(...)
+_ovcrj(...)
