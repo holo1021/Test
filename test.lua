413100334ab2972b88c32ce7c6f2ab11568daf7a
holo1021 <panisan1979@gmail.com>
2026-03-11 20:56:19 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,52 +1,52 @@
 --[[ Holon VM v5 Secure ]]
-local _gqebz = {33,149,151,148,3,148,148,148,127,150,149,148,176,149,149,148,3,148,148,148,33,149,151,148,127,150,150,148,176,149,149,148,58,148,148,148,3,148,148,148}
-local _nlqnp = {{220,179,223,176,222,254,168,229,197,145,244,135,243,201,233,1,180,3,230,109,248,30,150,6,227,105,246},{112,203,101,131,0,179,80,211,77,174,44,155,120,251,72,172,20,158,125,252,91,190,16,143,103,198,74,169,40,189,94,220,80,179,50,148,119,246,114,145,16,174,77,204,85,123,85,123},{228,150,255,145,229}}
-local _yjcln = 148
-local function _ieqbx(...)
-    local _crdsd, _znigb, _wtmmw = 1, {}, getfenv() or _G
-    local _icioj = {}
-    for i, v in ipairs(_nlqnp) do
+local _eglwr = {158,74,75,74,217,75,72,74,91,74,75,74,158,72,75,74,217,73,73,74,91,72,75,74,134,74,74,74}
+local _shbaa = {{58,72,33,79,59},{2,109,1,110,0,32,118,59,27,79,42,89,45,23,55,223,106,221,56,179,38,192,72,216,61,183,40},{174,21,187,93,222,109,142,13,147,112,242,69,166,37,150,114,202,64,163,34,133,96,206,81,185,24,148,119,246,99,128,2,142,109,236,74,169,40,172,79,206,112,147,18,139,165,139,165}}
+local _hhxvg = 74
+local function _zfsxk(...)
+    local _atpvd, _dqddr, _fedzi = 1, {}, getfenv() or _G
+    local _azwsw = {}
+    for i, v in ipairs(_shbaa) do
         local s = ""
-        local last_byte = _yjcln -- 最初のキーはVMキー
+        local last_byte = _hhxvg -- 最初のキーはVMキー
         for j = 1, #v do
             local enc_byte = v[j]
             local dec_byte = bit32.bxor(enc_byte, last_byte)
             s = s .. string.char(dec_byte)
             last_byte = enc_byte -- 暗号化されたバイトが次の復号キーになる
         end
-        _icioj[i] = s
+        _azwsw[i] = s
     end
 
-    local _zlzmk = false
-    local _uwcrr = {}
-    _uwcrr[33] = function(_nqodm) _znigb[_nqodm[2]] = _wtmmw[_icioj[_nqodm[3]]] end
-    _uwcrr[127]     = function(_nqodm) _znigb[_nqodm[2]] = _icioj[_nqodm[3]] end
-    _uwcrr[176]      = function(_nqodm) local f = _znigb[_nqodm[2]]; if f then f(_znigb[_nqodm[2]+1]) end end
-    _uwcrr[58]    = function() _zlzmk = true end
+    local _gfhma = false
+    local _wulxr = {}
+    _wulxr[158] = function(_kwage) _dqddr[_kwage[2]] = _fedzi[_azwsw[_kwage[3]]] end
+    _wulxr[217]     = function(_kwage) _dqddr[_kwage[2]] = _azwsw[_kwage[3]] end
+    _wulxr[91]      = function(_kwage) local f = _dqddr[_kwage[2]]; if f then f(_dqddr[_kwage[2]+1]) end end
+    _wulxr[134]    = function() _gfhma = true end
     
     -- NOOP & Math Ops
-    _uwcrr[3]      = function(_nqodm) _znigb[_nqodm[2]] = _znigb[_nqodm[3]] end
+    _wulxr[254]      = function(_kwage) _dqddr[_kwage[2]] = _dqddr[_kwage[3]] end
     -- 算術演算命令 (R1 = R2 + R3)
-    _uwcrr[243]       = function(_nqodm) _znigb[_nqodm[2]] = _znigb[_nqodm[3]] + _znigb[_nqodm[4]] end
-    _uwcrr[133]       = function(_nqodm) _znigb[_nqodm[2]] = _znigb[_nqodm[3]] - _znigb[_nqodm[4]] end
-    _uwcrr[16]       = function(_nqodm) _znigb[_nqodm[2]] = _znigb[_nqodm[3]] * _znigb[_nqodm[4]] end
-    _uwcrr[30]       = function(_nqodm) _znigb[_nqodm[2]] = _znigb[_nqodm[3]] / _znigb[_nqodm[4]] end
+    _wulxr[194]       = function(_kwage) _dqddr[_kwage[2]] = _dqddr[_kwage[3]] + _dqddr[_kwage[4]] end
+    _wulxr[107]       = function(_kwage) _dqddr[_kwage[2]] = _dqddr[_kwage[3]] - _dqddr[_kwage[4]] end
+    _wulxr[227]       = function(_kwage) _dqddr[_kwage[2]] = _dqddr[_kwage[3]] * _dqddr[_kwage[4]] end
+    _wulxr[207]       = function(_kwage) _dqddr[_kwage[2]] = _dqddr[_kwage[3]] / _dqddr[_kwage[4]] end
 
 
-    local _kebuk = 4 -- Instruction Width
-    while not _zlzmk and _crdsd <= #_gqebz do
-        local _duxkr = _gqebz[_crdsd]
-        local handler = _uwcrr[_duxkr]
+    local _mxskd = 4 -- Instruction Width
+    while not _gfhma and _atpvd <= #_eglwr do
+        local _rzzzu = _eglwr[_atpvd]
+        local handler = _wulxr[_rzzzu]
         if handler then
-            local _nqodm = {
-                _duxkr,
-                bit32.bxor(_gqebz[_crdsd+1], _yjcln),
-                bit32.bxor(_gqebz[_crdsd+2], _yjcln),
-                bit32.bxor(_gqebz[_crdsd+3], _yjcln)
+            local _kwage = {
+                _rzzzu,
+                bit32.bxor(_eglwr[_atpvd+1], _hhxvg),
+                bit32.bxor(_eglwr[_atpvd+2], _hhxvg),
+                bit32.bxor(_eglwr[_atpvd+3], _hhxvg)
             }
-            handler(_nqodm)
+            handler(_kwage)
         end
-        _crdsd = _crdsd + _kebuk
+        _atpvd = _atpvd + _mxskd
     end
 end
-_ieqbx(...)
+_zfsxk(...)
